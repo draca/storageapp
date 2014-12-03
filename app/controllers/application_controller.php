f06e960451d5b89b3099c36e6d78a31f4a080294
draca <draca@live.se>
2014-12-03 14:29:56 +0100

    some blackbirds tests

--- app/controllers/application_controller.php
+++ app/controllers/application_controller.php
@@ -11,7 +11,10 @@ class ApplicationController {
       $cfg->set_model_directory('../app/models');
       $cfg->set_connections(
         array(
-          'default' => 'mysql://'.database_default_username.''.(empty(database_default_password) ? '': ':'.database_default_password).'@'.database_default_hostname.'/'.database_default_database
+          
+
+          'default' => 'mysql://'.database_default_username.''.( !database_default_password ? '': ':'
+          .database_default_password).'@'.database_default_hostname.'/'.database_default_database
         )
       );
       $cfg->set_default_connection('default');
